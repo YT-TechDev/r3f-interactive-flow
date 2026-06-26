5dbc478892e5fe42c6b3313e0813f860cf37db4e
Yuto <msc_prop@proton.me>
2026-06-26 22:20:56 +0900

    fix: update vite basic example

--- examples/vite-basic/vite.config.ts
+++ examples/vite-basic/vite.config.ts
@@ -2,5 +2,13 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "r3f-interactive-flow": new URL(
+        "../../packages/r3f-interactive-flow/src/index.ts",
+        import.meta.url
+      ).pathname
+    }
+  }
 });
